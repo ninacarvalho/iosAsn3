45a9e5760d21904a82a04c459e0154d15fe3d831
Marina <marinacarvalho@protonmail.com>
2024-11-15 17:49:00 -0500

    removing stuff

--- Asn3/model/Question.swift
+++ Asn3/model/Question.swift
@@ -24,33 +24,6 @@ class QuestionManager {
     
     // Initializer to populate questionBank with sample questions
        public init() {
-           questionBank = [
-               Question(
-                   text: "What is the capital of France?",
-                   correctAnswer: "Paris",
-                   wrongAnswers: ["Berlin", "Madrid", "Rome"]
-               ),
-               Question(
-                   text: "What is the largest planet in our solar system?",
-                   correctAnswer: "Jupiter",
-                   wrongAnswers: ["Earth", "Mars", "Saturn"]
-               ),
-               Question(
-                   text: "Who wrote 'Romeo and Juliet'?",
-                   correctAnswer: "William Shakespeare",
-                   wrongAnswers: ["Charles Dickens", "Jane Austen", "Mark Twain"]
-               ),
-               Question(
-                   text: "Which element has the chemical symbol 'O'?",
-                   correctAnswer: "Oxygen",
-                   wrongAnswers: ["Osmium", "Ozone", "Oxygenium"]
-               ),
-               Question(
-                   text: "What is the speed of light?",
-                   correctAnswer: "299,792 km/s",
-                   wrongAnswers: ["150,000 km/s", "1,000,000 km/s", "3,000 km/s"]
-               )
-           ]
        }
     
     func addNewQuestion(_ question: Question) {
