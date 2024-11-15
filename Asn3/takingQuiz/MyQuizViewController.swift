6f24b381a3958002d9b601c3ad12339acb85985c
Marina <marinacarvalho@protonmail.com>
2024-11-15 18:21:42 -0500

    fixing question result page

--- Asn3/takingQuiz/MyQuizViewController.swift
+++ Asn3/takingQuiz/MyQuizViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class MyQuizViewController: UIViewController {
-   
+    
     @IBOutlet weak var questionLabel: UITextView!
     
     @IBOutlet weak var option1Button: UIButton!
@@ -20,7 +20,7 @@ class MyQuizViewController: UIViewController {
     
     @IBOutlet weak var previousButton: UIBarButtonItem!
     @IBOutlet weak var nextButton: UIBarButtonItem!
-    
+        
     private var currentQuestionIndex = 0
     private var answeredQuestions = Set<Int>()
     private var correctAnswersCount = 0
@@ -56,56 +56,59 @@ class MyQuizViewController: UIViewController {
         let isAnswered = answeredQuestions.contains(currentQuestionIndex)
         [option1Button, option2Button, option3Button, option4Button].forEach { $0?.isEnabled = !isAnswered }
         
+        updateProgress()
         previousButton.isEnabled = currentQuestionIndex > 0
-        nextButton.isEnabled = currentQuestionIndex < questionBank.count - 1
+        nextButton.isEnabled = currentQuestionIndex < questionBank.count
     }
     
-    
-      func updateProgress() {
-          progressView.progress = Float(answeredQuestions.count) / Float(questionBank.count)
+  func updateProgress() {
+      progressView.progress = Float(answeredQuestions.count) / Float(questionBank.count)
+  }
+  
+  @IBAction func optionButtonTapped(_ sender: UIButton) {
+      guard let selectedAnswer = sender.title(for: .normal) else { return }
+      
+      let correctAnswer = questionBank[currentQuestionIndex].correctAnswer
+      if selectedAnswer == correctAnswer {
+          correctAnswersCount += 1
       }
       
-      @IBAction func optionButtonTapped(_ sender: UIButton) {
-          guard let selectedAnswer = sender.title(for: .normal) else { return }
-          
-          let correctAnswer = questionBank[currentQuestionIndex].correctAnswer
-          if selectedAnswer == correctAnswer {
-              correctAnswersCount += 1
-          }
-          
-          answeredQuestions.insert(currentQuestionIndex)
-          
-          [option1Button, option2Button, option3Button, option4Button].forEach { $0?.isEnabled = false }
-          
-          updateProgress()
-      }
-    
-    
+      answeredQuestions.insert(currentQuestionIndex)
+      
+      [option1Button, option2Button, option3Button, option4Button].forEach { $0?.isEnabled = false }
+      
+      updateProgress()
+  }
+
+
     @IBAction func previousButtonTapped(_ sender: Any) {
         guard currentQuestionIndex > 0 else { return }
         currentQuestionIndex -= 1
         loadQuestion()
     }
-    
-    
+
+
     @IBAction func nextButtonTapped(_ sender: Any) {
-        guard currentQuestionIndex < questionBank.count - 1 else { return }
+        print("currentQuestionIndex: \(currentQuestionIndex) \n questionBank.count: \(questionBank.count )")
+        guard currentQuestionIndex < questionBank.count else { return }
         currentQuestionIndex += 1
         loadQuestion()
     }
-    
+
   
-  func showQuizResult() {
-      let percentageCorrect = Float(correctAnswersCount) / Float(questionBank.count) * 100
-      let message = "You answered \(percentageCorrect)% of questions correctly."
-      
-      let alert = UIAlertController(title: "Quiz Completed!", message: message, preferredStyle: .alert)
-      alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
-          self.navigationController?.popToRootViewController(animated: true)
-      }))
-      
-      present(alert, animated: true, completion: nil)
-  }
+    func showQuizResult() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let resultVC = storyboard.instantiateViewController(withIdentifier: "ResultViewController") as? ResultViewController {
+            let percentageCorrect = Float(correctAnswersCount) / Float(questionBank.count) * 100
+            resultVC.correctPercentage = percentageCorrect
+            
+            self.navigationController?.pushViewController(resultVC, animated: true)
+        } else {
+            print("Error: Unable to instantiate MyQuizResultViewController.")
+        }
+    }
+
+
 
 
 }
